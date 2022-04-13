e8b3b5103ac1a1dde6c3132882479800f7d48a3f
张朝 <35652672+1055923351@users.noreply.github.com>
2022-04-14 00:27:18 +0800

    Add files via upload

---
+++ day.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class day {
+
+    public static void main(String[] args) {
+        //定义一个数组
+        int [] infodigit = {12,33,32,56,7};
+        for (int i=0;i<infodigit.length-1;i++){
+           for (int s = 0;s <infodigit.length-1-i;s++){
+             if (infodigit[s] > infodigit[s+1]){
+                 int tmo = infodigit[s];
+                 infodigit[s]=infodigit[s+1];
+                 infodigit[s+1] = tmo;
+             }
+           }
+
+        }
+        System.out.print("从小到大排了"+Arrays.toString(infodigit));
+
+
+    }
+
+
+
+
+
+}
